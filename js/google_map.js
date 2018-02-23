33b676b620b5a313cf116c90a959a1084496d88a
Afuwape Ifeoluwa <hipheckt@gmail.com>
2018-02-23 06:29:58 +0100

    root

--- js/google_map.js
+++ js/google_map.js
@@ -4,13 +4,13 @@ function init() {
     // Basic options for a simple Google Map
     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
     // var myLatlng = new google.maps.LatLng(40.71751, -73.990922);
-    var myLatlng = new google.maps.LatLng(6.5945565, 3.2899886);
+    var myLatlng = new google.maps.LatLng(6.6321965, 3.3241008);
     // 39.399872
     // -8.224454
 
     var mapOptions = {
         // How zoomed in you want the map to start at (always required)
-        zoom: 7,
+        zoom: 18,
 
         // The latitude and longitude to center the map (always required)
         center: myLatlng,
@@ -29,7 +29,7 @@ function init() {
     // Create the Google Map using out element and options defined above
     var map = new google.maps.Map(mapElement, mapOptions);
 
-    var addresses = ['Brooklyn'];
+    var addresses = ['Iju'];
 
     for (var x = 0; x < addresses.length; x++) {
         $.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address=' + addresses[x] + '&sensor=false', null, function(data) {
